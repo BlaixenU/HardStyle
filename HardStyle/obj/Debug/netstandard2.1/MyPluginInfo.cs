a359ad21dedaae63a00305f3b928abc49e28b07b
BlaixenU <47916471+BlaixenU@users.noreply.github.com>
2026-08-03 15:24:40 +0500

    Delete HardStyle/obj directory

--- HardStyle/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace HardStyle;
-
-internal static class MyPluginInfo
-{
-    public const string PLUGIN_GUID = "com.blaixenu.hardstyle";
-    public const string PLUGIN_NAME = "HardStyle";
-    public const string PLUGIN_VERSION = "1.0.0";
-}
